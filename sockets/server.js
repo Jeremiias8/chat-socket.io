a231a96f84d7d618351238eed423e78fc33f8559
Jeremías <jeremiias122002@gmail.com>
2023-10-25 23:33:36 +0200

    configurando Socket

--- sockets/server.js
+++ sockets/server.js
@@ -1,1 +1,48 @@
 console.log('server works !');
+
+const express = require('express');
+const app = express();
+
+const http = require('http');
+
+const server = http.createServer(app);
+const port = 3000;
+
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+    console.log('new connection id: '+socket.id);
+});
+
+io.on('connection', (socket) => {
+
+    console.log('a user connected succesfully !');
+
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+    
+  });
+
+  server.listen(port, () => {
+    console.log(`listening on port: ${port}`);
+  });
+
+  io.on('connection', (socket) => {
+
+    socket.on('chat message', (msg) => {
+      console.log('message: ' + msg);
+      
+    });
+  })
+// fin de server config
+
+// ruta prueba
+app.get('/', (req, res) => {
+    res.send("Hey, soy la ruta principal del Chat con NodeJS !");
+});
+
+app.get('/index', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
